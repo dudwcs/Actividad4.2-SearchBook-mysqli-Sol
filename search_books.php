3a57a5862d0817aaf48b3ad0267c55e1b32aa59e
dwcs-2324 <dwcs2324@gmail.com>
2023-11-20 12:25:05 +0100

    A4.2-enunciado mysqli

--- search_books.php
+++ search_books.php
@@ -23,48 +23,36 @@ if (isset($_GET["busqueda"])) {
     $terminos_busqueda = $_GET["busqueda"];
     if (trim($terminos_busqueda) !== "") {
 
-        require_once "MyPDO.php";
+        require_once "connection.php";
 
         try {
-            $con = new MyPDO();
+            $con =  getConnection();
 
             //En la bd bookdb no importan mayúsculas/minúsculas porque está usando collation caseinsensitive, pero no está demás que nuestro código no dependa de la collation de la base de datos
-            // $stmt = $con->prepare("select title as resultado from books where UPPER(title) like :busqueda 
-            //     union 
-            //     select TRIM(Concat(coalesce(first_name, '') , coalesce(middle_name, ' '), coalesce(last_name, '')))
-            //      as resultado from authors where first_name like :busqueda;");
+            $stmt = $con->prepare("select title as resultado from books where UPPER(title) like ?
+                union 
+                select TRIM(Concat(coalesce(first_name, '') , coalesce(middle_name, ' '), coalesce(last_name, '')))
+                 as resultado from authors where first_name like ?;");
 
-            // No tan útil para obtener los resultados y mostrarlos en html:
-            // SELECT title, first_name, middle_name, last_name
-            // FROM books b
-            // LEFT JOIN book_authors ba ON b.book_id = ba.book_id
-            // LEFT JOIN authors a ON ba.author_id = a.author_id
-            // WHERE UPPER(title) like '%a%' OR first_name LIKE '%a%';
-
-            
             $filtro = "%" . strtoupper($terminos_busqueda) . "%";
-            $stmt->bindParam("busqueda", $filtro);
-
-            // echo "<pre>";
-            // $stmt->debugDumpParams();
-            // echo "</pre>";
-
+            $stmt->bind_param("ss", $filtro, $filtro);
             $stmt->execute();
 
-            echo "<pre>";
-            $stmt->debugDumpParams();
-            echo "</pre>";
+            $resultado = $stmt->get_result();
 
             $counter = 0;
-            while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+            //fetch_assoc devuelve:
+            // array asoc
+            //null si no hay más filas
+            //false si falla algo
+            while (($row = $resultado->fetch_assoc())) {
                 $counter++;
                 if ($counter == 1) {
                     echo "<ol>";
                 }
                 echo "<li>" . $row["resultado"] . "</li>";
-              
             }
-            if($counter >0){
+            if ($counter > 0) {
                 echo "</ol>";
             }
             if ($counter == 0) {
@@ -74,8 +62,12 @@ if (isset($_GET["busqueda"])) {
             echo "<p>Ha ocurrido una excepción: " . $e->getMessage() . "</p>";
         }
         //Cerramos los recursos
+        //Open non-persistent MySQL connections and result sets are automatically closed when their objects are destroyed. Explicitly closing open connections and freeing result sets is optional.
         $con = null;
-        $stmt = null;
+        if ($stmt != null)
+            $stmt->close();
+        if ($resultado != null)
+            $resultado->free();
     } else {
         echo "<p> Introduzca una cadena no vacía </p>";
     }
